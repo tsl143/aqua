b4b899009e232a52342b42134e1e1a1172b58a82
dipali goel <dipa.goel@gmail.com>
2014-06-03 23:21:04 +0530

    First pages commit

---
+++ header.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<!-- Website template by freewebsitetemplates.com -->
+<html>
+<head>
+	<title>Aquamist - More than just a coffee</title>
+	<meta charset="UTF-8">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<!--[if IE 7]>
+		<link rel="stylesheet" type="text/css" href="css/ie7.css">	
+	<![endif]-->
+	<!--[if IE 6]>
+		<link rel="stylesheet" type="text/css" href="css/ie6.css">		
+	<![endif]-->
+        <script src="js/jquery-2.1.0.min.js"></script>
+        <script src="js/main.js"></script>
+</head>
+<body>
+<div id="header"> <!-- start of header -->
+		<span class="signboard"></span>
+		<a href="index.php" id="logo"></a>
+                <a href="index.php" id="deliver"></a>
+                <a href="https://www.facebook.com/AQUAMISTCAFE" id="fbook" target="_blank"></a>
+                <a href="https://plus.google.com/+AquamistCafeJaipur/posts" id="gplus" target="_blank"></a>
+                <div class="hideit deliverypopup">For delivery 9983344433</div>
+<!--		<a href="menu.php" class="tagline"></a> -->
+	</div> <!-- end of header -->
